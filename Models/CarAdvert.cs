011328d2dac4983e11921e369767d1d430eb5d44
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2025-05-15 22:55:40 +0200

    redesigned architecture of project

---
+++ Models/CarAdvert.cs
@@ -0,0 +1,7 @@
+public class CarAdvert
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string Status { get; set; } // "New", "Used", "Pre-owned"
+}
